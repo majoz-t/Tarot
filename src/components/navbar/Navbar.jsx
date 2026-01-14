7c983cde3ee6ca3e976550dd1d4de90df7c09b24
majoz-t <majozta@gmail.com>
2026-01-14 12:41:15 +0100

    feat: create Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import "./navbar.css";
 
-
 const Navbar = () => {
   return (
     <>
       <nav>
-        <img src={image-7.png} alt="logo-nav" />
+        <img src="././public/image-7.png" alt="logo-nav" />
         <div className="title-nav">SAKURA TAROT</div>
       </nav>
     </>
