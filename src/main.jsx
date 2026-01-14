e1cef016ae3596c4535cd19df2690918e1069ceb
@Eva-87 <mariaeva_martin@hotmail.com>
2026-01-14 14:30:55 +0100

    testing routes ,last changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { RouterProvider } from 'react-router/dom'
+import {router} from '../src/router/router.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
